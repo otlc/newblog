f187771bc04418decfa7fe03eeca8f2e4cf53c52
OTLC <onthelinecoders@gmail.com>
2015-12-21 23:35:47 -0500

    Add validation to post form

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,16 +9,22 @@ class PostsController < ApplicationController
 	end
 
 	def new
-
+		@post = Post.new
 	end
 
 	def create
 		# first we create a post in memory
 		@post = Post.new(post_params)
 		# then we save the post to the db
-		@post.save
-		# finally we redirect back to the new post
-		redirect_to @post
+		if @post.save
+			# finally we redirect back to the new post
+			# redirect does an entirely new http refresh
+			redirect_to @post
+		else
+			# else we render the new method 
+			# using render it does do an entirely new http refresh and will keep all entered content in-place on form on an error.
+			render 'new'
+		end
 	end
 
 	private
@@ -26,4 +32,4 @@ class PostsController < ApplicationController
 		def post_params
 			params.require(:post).permit(:title, :body)
 		end
-end
+	end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,5 @@
 class Post < ActiveRecord::Base
+# adding validations to our model. minumum title of 5 chars and body must contain content.
+	validates :title, presence: true, length: {minimum: 5}
+	validates :body, presence: true
 end
