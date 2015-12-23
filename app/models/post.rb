79f28ed9db28de3903b344736bafa8afce81d302
OTLC <onthelinecoders@gmail.com>
2015-12-23 11:46:12 -0500

    Add comments capability

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,6 @@
 class Post < ActiveRecord::Base
 # adding validations to our model. minumum title of 5 chars and body must contain content.
+	has_many :comments, dependent: :destroy
 	validates :title, presence: true, length: {minimum: 5}
 	validates :body, presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
 # Add default routes for posts
- resources :posts
+ resources :posts do
+ 	# add nested resources
+ 	resources :comments
+ end
 # Also add posts as the index for our application. Add the posts controller index action as the default/root for our application landing page.
  root "posts#index"
 end
